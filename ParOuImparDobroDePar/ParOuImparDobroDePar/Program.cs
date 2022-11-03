421043c70e3abcad0a7fa68b4d04e956cb05ba85
Carlos Alberto <92952406+Caalb@users.noreply.github.com>
2022-11-03 17:36:11 -0300

    add files via upload

---
+++ ParOuImparDobroDePar/ParOuImparDobroDePar/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ParOuImparDobroDePar {
+    internal class Program {
+        static void Main(string[] args) {
+
+            int pares = 0;
+            int impares = 0;
+
+            do
+            {
+                Console.WriteLine("Entre com um número inteiro:");
+                int num = int.Parse(Console.ReadLine());
+
+                if(num % 2 == 0)
+                {
+                    pares++;
+                } else impares++;
+
+                Console.WriteLine("Quantidade de pares: " + pares);
+                Console.WriteLine("Quantidade de impares: " + impares);
+            } while (impares * 2 != pares);
+
+            if(x > y)
+            {
+                cw("X é maior")
+            }
+        }
+    }
+}
